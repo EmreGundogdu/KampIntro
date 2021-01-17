c358c97d9eb9023da83ca99b7db60177325f0c98
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-01-17 17:46:02 +0300

    Add project files.

---
+++ Metodlar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Çilek";
+            urun2.Fiyati = 10;
+            urun2.Aciklama = "Çilek gibi çilek";
+
+            Urun urun3 = new Urun();
+            urun3.Adi = "Karpuz";
+            urun3.Fiyati = 50;
+            urun3.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2, urun3 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("***********");
+            }
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+
+            sepetManager.Ekle2("Armut", "Yaz meyvesi", 12);
+
+        }
+    }
+}
---
+++ Metodlar/SepetManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine(urun + "sepete eklendi");
+        }
+        public void Ekle2(string urunAdi, string aciklama, double fiyat)
+        {
+            Console.WriteLine(urunAdi + " sepete eklendi");
+        }
+    }
+}
